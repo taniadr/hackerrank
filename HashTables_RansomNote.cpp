a3678d0d02cb10df1270b87d719e0d8bbf3d4321
tania <taniadiasr@bitbucket.org>
2017-04-17 12:17:54 -0300

    code cleaning at ransonNote

--- HashTables_RansomNote.cpp
+++ HashTables_RansomNote.cpp
@@ -28,28 +28,7 @@ Coder: taniadr // https://github.com/taniadr/hackerrank
 #include <unordered_map>
 
 using namespace std;
-/*
-bool ransom_note(vector<string> magazine, vector<string> ransom) {	//Its working but its getting time out
-	
-	vector<int> counts;
-	int quant=0;
-	int quant2=0;
-	int s1=magazine.size();
-	int s2=ransom.size();
-	if (s2 > s1)	//a nota é mais larga que a quant. de palavras disponíveis
-		return false;
-	
-	for (int i=0; i<ransom.size(); i++ )	//veremos se as palavras da msg tem na revista 
-	{
-		quant = count(magazine.begin(), magazine.end(), ransom[i]);	//e se em quantidade necessaria
-		quant2 = count(ransom.begin(), ransom.end(), ransom[i]);
-		
-		if (quant < quant2) { 		//se nao tem o numero minimo necessario, return false
-			return false;
-		}		
-	}
-	return true;
-}*/
+
 typedef unordered_map<string,int> charmap;
 typedef unordered_map<string,int>::const_iterator itera;
 
